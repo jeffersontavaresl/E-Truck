6076f0a98cd968e52be3f4c8c4390a4e8396b924
Jefferson Tavares <83378931+jeffersontavaresl@users.noreply.github.com>
2021-07-29 10:46:16 -0300

    Update Caixa.java

--- GestorFoodTruck2/src/java/modelos/Caixa.java
+++ GestorFoodTruck2/src/java/modelos/Caixa.java
@@ -3,18 +3,77 @@ package modelos;
 public class Caixa {
 
     private int id;
-    private int idMesa;
+    private int nrMesa;
     private String formPagamento;
     private float preco;
     private int codProduto;
 
-    public void ConsultarPedido() {
-        // TODO implement here
+    public Caixa ConsultarPedido(int nrMesa) {
+        Connection con = Conexao.conectar();
+        String  sql  = "SELECT codproduto, nomeproduto, tipoproduto, preco ";
+                sql += "FROM pedidocliente ";
+                sql += "WHERE nrmesa = ?";
+        Caixa caixa = null;
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt(1, nrmesa);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                caixa = new Caixa();
+                caixa.setNrMesa(nrMesa);
+                caixa.setCodProduto(rs.getInt("codproduto"));
+                caixa.setNomeProduto(rs.getString("nomeproduto"));
+                caixa.setTipoProduto(rs.getString("tipoproduto"));
+                caixa.setPreco(rs.getFloat("preco"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
+        return cardapio;
+    }
+    
+    public List<Caixa> lovItem() {
+        List<Caixa> lista = new ArrayList<>();
+        Connection con = Conexao.conectar();
+        String  sql  = "SELECT codproduto, nomeproduto, tipoproduto, preco ";
+                sql += "FROM pedidocliente ";
+                sql += "ORDER BY nrmesa";
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+             while (rs.next()) {
+                Caixa caixa = new Caixa();
+                caixa.setNrMesa(nrMesa);
+                caixa.setCodProduto(rs.getInt("codproduto"));
+                caixa.setNomeProduto(rs.getString("nomeproduto"));
+                caixa.setTipoProduto(rs.getString("tipoproduto"));
+                caixa.setPreco(rs.getFloat("preco"));
+                lista.add(caixa);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
+        return lista;
     }
 
-    public void FecharPedido() {
-        // TODO implement here
+    public boolean FecharPedido() {
+        Connection con = Conexao.conectar();
+        String  sql  = "UPDATE pedidocliente";
+                sql += " SET status   = ?,";
+                sql += " WHERE nrmesa  = ? ";
+
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, this.status);
+            stm.setInt   (2, this.nrMesa);
+            stm.execute();
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+            return false;
+        }
+        return true;
     }
+    
 
     public int getId() {
         return id;
@@ -24,12 +83,12 @@ public class Caixa {
         this.id = id;
     }
 
-    public int getIdMesa() {
-        return idMesa;
+    public int getNrMesa() {
+        return nrMesa;
     }
 
-    public void setIdMesa(int idMesa) {
-        this.idMesa = idMesa;
+    public void setNrMesa(int nrMesa) {
+        this.nrMesa = nrMesa;
     }
 
     public String getFormPagamento() {
