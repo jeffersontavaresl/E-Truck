0816690e10882064c2c71b67103c90a5400d37ce
Jefferson Tavares <83378931+jeffersontavaresl@users.noreply.github.com>
2021-08-03 10:50:00 -0300

    Update Caixa.java

--- GestorFoodTruck2/src/java/modelos/Caixa.java
+++ GestorFoodTruck2/src/java/modelos/Caixa.java
@@ -6,77 +6,24 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import utils.Conexao;
-public class Caixa {
+public class Cardapio extends Administrador {
 
     private int id;
-    private int codPedido;
-    private String formPagamento;
-    private float preco;
-    private int codProduto;
     private String produto;
-    private String statusPagto;
-
-    public Caixa consultarPedido(int codPedido) {
-        Connection con = Conexao.conectar();
-        String  sql  = "SELECT a.codpedido, a.codproduto, a.produto, c.preco ";
-                sql += "FROM pedidocliente a, cardapio c ";
-                sql += "WHERE codpedido  = ? ";
-                sql += "AND statusPagto = ? ";
-                sql += "AND a.codproduto = c.codproduto";
-        Caixa caixa = null;
-        try {
-            PreparedStatement stm = con.prepareStatement(sql);
-            stm.setInt(1, codPedido);
-            stm.setString(2, this.statusPagto);
-            ResultSet rs = stm.executeQuery();
-            if (rs.next()) {
-                caixa = new Caixa();
-                caixa.setCodPedido(rs.getInt("codpedido"));
-                caixa.setCodProduto(rs.getInt("codproduto"));
-                caixa.setProduto(rs.getString("produto"));
-                caixa.setPreco(rs.getFloat("preco"));
-            }
-        } catch (SQLException ex) {
-            System.out.println("Erro: " + ex.getMessage());
-        }
-        return caixa;
-    }
+    private float preco;
+    private int idEstoque;
+    private int codProduto;
     
-    public List<Caixa> lovItem() {
-        List<Caixa> lista = new ArrayList<>();
+        public boolean cadastrarItem() {
+        String  sql  = "INSERT INTO cardapio (codproduto, produto, preco)";
+                sql += "VALUES(?,?,?)";
         Connection con = Conexao.conectar();
-        String  sql  = "SELECT a.codpedido, a.codproduto, a.produto, ";
-                sql += "c.preco, a.statuspagto ";
-                sql += "FROM pedidocliente a, cardapio c";
-                sql += "ORDER BY codpedido";
-        try {
-            PreparedStatement stm = con.prepareStatement(sql);
-            ResultSet rs = stm.executeQuery();
-             while (rs.next()) {
-                Caixa caixa = new Caixa();
-                caixa.setCodPedido(rs.getInt("codpedido"));
-                caixa.setCodProduto(rs.getInt("codproduto"));
-                caixa.setProduto(rs.getString("produto"));
-                caixa.setPreco(rs.getFloat("preco"));
-                caixa.setStatusPagto(rs.getString("statuspagto"));
-                lista.add(caixa);
-            }
-        } catch (SQLException ex) {
-            System.out.println("Erro: " + ex.getMessage());
-        }
-        return lista;
-    }
-
-    public boolean fecharPedido() {
-        Connection con = Conexao.conectar();
-        String  sql  = "UPDATE pedidocliente";
-                sql += " SET statuspagto   = ?,";
-                sql += " WHERE codpedido  = ? ";
 
         try {
             PreparedStatement stm = con.prepareStatement(sql);
-            stm.setString(1, this.statusPagto);
-            stm.setInt   (2, this.codPedido);
+            stm.setInt   (1, this.codProduto);
+            stm.setString(2, this.produto);
+            stm.setFloat (3, this.preco);
             stm.execute();
         } catch (SQLException ex) {
             System.out.println("Erro: " + ex.getMessage());
@@ -84,32 +31,113 @@ public class Caixa {
         }
         return true;
     }
+
+    public boolean alterarItem() {
+        Connection con = Conexao.conectar();
+        String  sql  = "UPDATE cardapio";
+                sql += " SET codproduto = ?,";
+                sql += "     produto    = ?,";
+                sql += "     preco      = ? ";
+                sql += " WHERE codproduto  = ? ";
+
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt   (1, this.codProduto);
+            stm.setString(2, this.produto);
+            stm.setFloat (3, this.preco);
+            stm.setInt   (4, this.codProduto);
+            stm.execute();
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public boolean excluirItem() {
+        Connection con = Conexao.conectar();
+        String  sql  = "DELETE FROM cardapio ";
+                sql += " WHERE codproduto = ?";
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt(1, this.codProduto);
+            stm.execute();
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public Cardapio consultarItem(int codProduto) {
+        Connection con = Conexao.conectar();
+        String  sql  = "SELECT codproduto, produto, preco, idEstoque ";
+                sql += "FROM cardapio ";
+                sql += "WHERE codproduto = ?";
+        Cardapio cardapio = null;
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt(1, this.codProduto);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                cardapio = new Cardapio();
+                cardapio.setCodProduto(rs.getInt("codproduto"));
+                cardapio.setProduto(rs.getString("produto"));
+                cardapio.setPreco(rs.getFloat("preco"));
+                cardapio.setIdEstoque(rs.getInt("idEstoque"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
+        return cardapio;
+    }
+    
+    public List<Cardapio> lovCardapio() {
+        List<Cardapio> lista = new ArrayList<>();
+        Connection con = Conexao.conectar();
+        String  sql  = "SELECT codproduto, produto, preco, idEstoque ";
+                sql += "FROM cardapio ";
+                sql += "ORDER BY codproduto";
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+             while (rs.next()) {
+                Cardapio cardapio = new Cardapio();
+                cardapio.setCodProduto(rs.getInt("codproduto"));
+                cardapio.setProduto(rs.getString("produto"));
+                cardapio.setPreco(rs.getFloat("preco"));
+                cardapio.setIdEstoque(rs.getInt("idEstoque"));
+                lista.add(cardapio);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
+        return lista;
+    }
     
     /* ÁREA DE GETTERS E SETTERS */ 
     
+    @Override
     public int getId() {
         return id;
     }
 
+
+    @Override
     public void setId(int id) {
         this.id = id;
     }
 
-    public int getCodPedido() {
-        return codPedido;
+
+    public String getProduto() {
+        return produto;
     }
 
-    public void setCodPedido(int codPedido) {
-        this.codPedido = codPedido;
+ 
+    public void setProduto(String produto) {
+        this.produto = produto;
     }
 
-    public String getFormPagamento() {
-        return formPagamento;
-    }
-
-    public void setFormPagamento(String formPagamento) {
-        this.formPagamento = formPagamento;
-    }
 
     public float getPreco() {
         return preco;
@@ -117,6 +145,14 @@ public class Caixa {
 
     public void setPreco(float preco) {
         this.preco = preco;
+    }
+
+    public int getIdEstoque() {
+        return idEstoque;
+    }
+
+    public void setIdEstoque(int idEstoque) {
+        this.idEstoque = idEstoque;
     }
     
     public int getCodProduto() {
@@ -126,21 +162,4 @@ public class Caixa {
     public void setCodProduto(int codProduto) {
         this.codProduto = codProduto;
     }
-
-    public String getProduto() {
-        return produto;
-    }
-
-    public void setProduto(String produto) {
-        this.produto = produto;
-    }
-
-    public String getStatusPagto() {
-        return statusPagto;
-    }
-
-    public void setStatusPagto(String statusPagto) {
-        this.statusPagto = statusPagto;
-    }
-
 }
