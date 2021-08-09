80abc7d04c00ad41e1b14a62e0955283fdf8c4bb
Jefferson Tavares <83378931+jeffersontavaresl@users.noreply.github.com>
2021-08-09 09:56:36 -0300

    Update PedidoCliente.java

--- GestorFoodTruck2/src/java/modelos/PedidoCliente.java
+++ GestorFoodTruck2/src/java/modelos/PedidoCliente.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import utils.Conexao;
 
-public class PedidoCliente {
+public class PedidoCliente extends Cardapio{
 
     
     private int codPedido;
@@ -18,6 +18,7 @@ public class PedidoCliente {
     private String observacao;
     private String statusPagto;
     private String statusPedido;
+    private String mesa;
 
     public boolean realizarPedido() {
         String  sql  = "INSERT INTO pedidocliente (codmesa, ";
@@ -91,23 +92,25 @@ public class PedidoCliente {
      public List<PedidoCliente> consultaPedido(int pCodMesa) {
         List<PedidoCliente> lista = new ArrayList<>();
         Connection con = Conexao.conectar();
-        String  sql  = "SELECT codpedido, codproduto, produto, observacao, ";
-                sql += "statuspagto,statuspedido, codmesa ";
-                sql += "FROM pedidocliente ";
+        String  sql  = "SELECT a.codpedido, a.codproduto, a.produto, a.codmesa, ";
+                sql += "a.observacao, a.statuspagto, a.statuspedido, c.preco ";
+                sql += "FROM pedidocliente a, cardapio c ";
                 sql += "WHERE codmesa = ? ";
+                sql += "AND a.codproduto = c.codproduto ";
         try {
             PreparedStatement stm = con.prepareStatement(sql);
             stm.setInt(1, pCodMesa);
             ResultSet rs = stm.executeQuery();
             while (rs.next()) {
                 PedidoCliente pedcliente = new PedidoCliente();
-                pedcliente.setCodPedido(rs.getInt("codpedido"));
+                pedcliente.setCodProduto(rs.getInt("codpedido"));
                 pedcliente.setCodProduto(rs.getInt("codproduto"));
                 pedcliente.setProduto(rs.getString("produto"));
+                pedcliente.setCodMesa(rs.getInt("codmesa"));
                 pedcliente.setObservacao(rs.getString("observacao"));
                 pedcliente.setStatusPagto(rs.getString("statuspagto"));
                 pedcliente.setStatusPedido(rs.getString("statuspedido"));
-                pedcliente.setCodMesa(rs.getInt("codmesa"));
+                pedcliente.setPreco(rs.getFloat("preco"));
                 lista.add(pedcliente);
             }
         } catch (SQLException ex) {
@@ -119,8 +122,10 @@ public class PedidoCliente {
     public List<PedidoCliente> lovPedidos() {
         List<PedidoCliente> lista = new ArrayList<>();
         Connection con = Conexao.conectar();
-        String  sql  = "SELECT codpedido, produto, codmesa";
-                sql += "FROM pedidocliente ";
+        String  sql  = "SELECT a.codpedido, a.codproduto, a.produto, a.codmesa, ";
+                sql += "a.observacao, a.statuspagto, a.statuspedido, c.preco ";
+                sql += "FROM pedidocliente a, cardapio c ";
+                sql += "WHERE a.codproduto = c.codproduto ";
                 sql += "ORDER BY codmesa";
         try {
             PreparedStatement stm = con.prepareStatement(sql);
@@ -128,8 +133,34 @@ public class PedidoCliente {
              while (rs.next()) {
                 PedidoCliente pedcliente = new PedidoCliente();
                 pedcliente.setCodProduto(rs.getInt("codpedido"));
+                pedcliente.setCodProduto(rs.getInt("codproduto"));
                 pedcliente.setProduto(rs.getString("produto"));
                 pedcliente.setCodMesa(rs.getInt("codmesa"));
+                pedcliente.setObservacao(rs.getString("observacao"));
+                pedcliente.setStatusPagto(rs.getString("statuspagto"));
+                pedcliente.setStatusPedido(rs.getString("statuspedido"));
+                pedcliente.setPreco(rs.getFloat("preco"));
+                lista.add(pedcliente);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
+        return lista;
+    }
+    
+    public List<PedidoCliente> lovMesa() {
+        List<PedidoCliente> lista = new ArrayList<>();
+        Connection con = Conexao.conectar();
+        String  sql  = "SELECT codmesa, mesa ";
+                sql += "FROM mesa ";
+                sql += "ORDER BY codmesa";
+        try {
+            PreparedStatement stm = con.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+             while (rs.next()) {
+                PedidoCliente pedcliente = new PedidoCliente();
+                pedcliente.setCodMesa(rs.getInt("codmesa"));
+                pedcliente.setMesa(rs.getString("mesa"));
                 lista.add(pedcliente);
             }
         } catch (SQLException ex) {
@@ -211,6 +242,14 @@ public class PedidoCliente {
 
     public void setStatusPedido(String statusPedido) {
         this.statusPedido = statusPedido;
+    }
+
+    public String getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(String mesa) {
+        this.mesa = mesa;
     }
     
 }
