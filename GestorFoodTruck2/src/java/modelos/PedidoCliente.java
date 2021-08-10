8f79c3d5da619ac6d294778f344448fbfbc87a9c
Jefferson Tavares <83378931+jeffersontavaresl@users.noreply.github.com>
2021-08-10 10:42:22 -0300

    Update PedidoCliente.java

--- GestorFoodTruck2/src/java/modelos/PedidoCliente.java
+++ GestorFoodTruck2/src/java/modelos/PedidoCliente.java
@@ -9,12 +9,10 @@ import java.util.List;
 import utils.Conexao;
 
 public class PedidoCliente extends Cardapio{
-
     
     private int codPedido;
     private int codProduto;
     private int codMesa;
-    private String produto;
     private String observacao;
     private String statusPagto;
     private String statusPedido;
@@ -22,14 +20,14 @@ public class PedidoCliente extends Cardapio{
 
     public boolean realizarPedido() {
         String  sql  = "INSERT INTO pedidocliente (codmesa, ";
-                sql += " produto, observacao, statuspagto, statuspedido) ";
+                sql += " codproduto, observacao, statuspagto, statuspedido) ";
                 sql += "VALUES(?,?,?,?,?)";
         Connection con = Conexao.conectar();
 
         try {
             PreparedStatement stm = con.prepareStatement(sql);
             stm.setInt   (1, this.codMesa);
-            stm.setString(2, this.produto);
+            stm.setInt   (2, this.codProduto);
             stm.setString(3, this.observacao);
             stm.setString(4, this.statusPagto);
             stm.setString(5, this.statusPedido);
@@ -45,16 +43,14 @@ public class PedidoCliente extends Cardapio{
         Connection con = Conexao.conectar();
         String  sql  = "UPDATE pedidocliente ";
                 sql += " SET codproduto    = ?, ";
-                sql += "     produto      = ?, ";
                 sql += "     observacao   = ? ";
                 sql += " WHERE codmesa  = ? ";
 
         try {
             PreparedStatement stm = con.prepareStatement(sql);
             stm.setInt   (1, this.codProduto);
-            stm.setString(2, this.produto);
-            stm.setString(3, this.observacao);
-            stm.setInt   (4, this.codMesa);
+            stm.setString(2, this.observacao);
+            stm.setInt   (3, this.codMesa);
             stm.execute();
         } catch (SQLException ex) {
             System.out.println("Erro: " + ex.getMessage());
@@ -65,10 +61,11 @@ public class PedidoCliente extends Cardapio{
 
     public PedidoCliente consultarPedido(int pCodMesa) {
         Connection con = Conexao.conectar();
-        String  sql  = "SELECT codproduto, produto, observacao, ";
-                sql += " statuspagto, statuspedido ";
-                sql += "FROM pedidocliente ";
+        String  sql  = "SELECT a.codpedido, a.codproduto, c.produto, ";
+                sql += "a.observacao, a.statuspagto, a.statuspedido ";
+                sql += "FROM pedidocliente a, cardapio c";
                 sql += "WHERE codmesa = ? ";
+                sql += "AND a.codproduto = c.codproduto";
         PedidoCliente pedcliente = null;
         try {
             PreparedStatement stm = con.prepareStatement(sql);
@@ -76,12 +73,12 @@ public class PedidoCliente extends Cardapio{
             ResultSet rs = stm.executeQuery();
             if (rs.next()) {
                 pedcliente = new PedidoCliente();
+                pedcliente.setCodPedido(rs.getInt("codpedido"));
                 pedcliente.setCodProduto(rs.getInt("codproduto"));
-                pedcliente.setProduto(rs.getString("produto"));
+                pedcliente.setDescProduto(rs.getString("descproduto"));
                 pedcliente.setObservacao(rs.getString("observacao"));
                 pedcliente.setStatusPagto(rs.getString("statuspagto"));
                 pedcliente.setStatusPedido(rs.getString("statuspedido"));
-
             }
         } catch (SQLException ex) {
             System.out.println("Erro: " + ex.getMessage());
@@ -105,7 +102,7 @@ public class PedidoCliente extends Cardapio{
                 PedidoCliente pedcliente = new PedidoCliente();
                 pedcliente.setCodProduto(rs.getInt("codpedido"));
                 pedcliente.setCodProduto(rs.getInt("codproduto"));
-                pedcliente.setProduto(rs.getString("produto"));
+                pedcliente.setDescProduto(rs.getString("descproduto"));
                 pedcliente.setCodMesa(rs.getInt("codmesa"));
                 pedcliente.setObservacao(rs.getString("observacao"));
                 pedcliente.setStatusPagto(rs.getString("statuspagto"));
@@ -134,7 +131,7 @@ public class PedidoCliente extends Cardapio{
                 PedidoCliente pedcliente = new PedidoCliente();
                 pedcliente.setCodProduto(rs.getInt("codpedido"));
                 pedcliente.setCodProduto(rs.getInt("codproduto"));
-                pedcliente.setProduto(rs.getString("produto"));
+                pedcliente.setDescProduto(rs.getString("descproduto"));
                 pedcliente.setCodMesa(rs.getInt("codmesa"));
                 pedcliente.setObservacao(rs.getString("observacao"));
                 pedcliente.setStatusPagto(rs.getString("statuspagto"));
@@ -172,7 +169,7 @@ public class PedidoCliente extends Cardapio{
     public boolean cancelarPedido() {
         Connection con = Conexao.conectar();
         String  sql  = "DELETE FROM pedidocliente ";
-                sql += " WHERE codmesa = ?";
+                sql += " WHERE codmesa = ? ";
                 sql += " AND codproduto = ?";
         try {
             PreparedStatement stm = con.prepareStatement(sql);
@@ -196,28 +193,12 @@ public class PedidoCliente extends Cardapio{
         this.codPedido = codPedido;
     }
 
-    public int getCodProduto() {
-        return codProduto;
-    }
-
-    public void setCodProduto(int codProduto) {
-        this.codProduto = codProduto;
-    }
-
     public int getCodMesa() {
         return codMesa;
     }
 
     public void setCodMesa(int codMesa) {
         this.codMesa = codMesa;
-    }
-
-    public String getProduto() {
-        return produto;
-    }
-
-    public void setProduto(String produto) {
-        this.produto = produto;
     }
 
     public String getObservacao() {
